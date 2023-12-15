bfae83dedd7e4c6e689a67e90ac18d5ee3a56935
Klajoe <parlar_ar@hotmail.com>
2023-12-15 13:11:26 +0300

    Hard coded for see the problem

--- main.js
+++ main.js
@@ -1,51 +1,56 @@
-// Importing the necessary modules
-const fs = require('fs');
-const Papa = require('papaparse');
+// Hardcoded data simulating the contents of the CSV files
+const classList = [
+    { StudentID: 1001, ProfessorName: 'John Smith', CourseID: 'CENG201', ExamDuration: 60 },
+    { StudentID: 1002, ProfessorName: 'Jane Doe', CourseID: 'MATH202', ExamDuration: 120 },
+    { StudentID: 1003, ProfessorName: 'John Smith', CourseID: 'CENG203', ExamDuration: 60 },
+    // ... add more entries as needed
+];
 
-// Function to read and parse CSV file
-function readCSVFile(filePath) {
-    const fileContent = fs.readFileSync(filePath, 'utf8');
-    return Papa.parse(fileContent, { header: true }).data;
-}
+const classroomCapacities = [
+    { RoomID: 'CENG201', Capacity: 40 },
+    { RoomID: 'MATH202', Capacity: 30 },
+    { RoomID: 'CENG203', Capacity: 50 },
+    // ... add more entries as needed
+];
 
-// Function to check if the slot is available considering the new constraints
-function isSlotAvailable(schedule, classInfo, slot, classroomCapacities) {
-    // Check if the professor is already scheduled
+// Function to check if the slot is available
+function isSlotAvailable(schedule, classInfo, slot) {
+    // Check professor and student conflicts
     for (let key in schedule) {
-        if (schedule[key].time === slot && schedule[key].professor === classInfo.ProfessorName) {
-            return false; // Professor conflict
+        if (schedule[key].time === slot && (schedule[key].professor === classInfo.ProfessorName || schedule[key].student === classInfo.StudentID)) {
+            return false; // Conflict found
         }
     }
 
     // Check classroom capacity constraint
-    let capacity = classroomCapacities.find(room => room.RoomID === classInfo.RoomID).Capacity;
-    if (capacity / 2 < parseInt(classInfo.ExamDuration)) {
+    let capacity = classroomCapacities.find(room => room.RoomID === classInfo.CourseID).Capacity;
+    if (capacity / 2 < classInfo.ExamDuration) {
         return false; // Classroom capacity constraint not met
     }
 
     return true; // Slot is available
 }
 
-// Updated backtracking scheduler
-function scheduleExams(classList, examSlots, classroomCapacities) {
+// Backtracking scheduler function
+function scheduleExams(classList, examSlots) {
     let schedule = {};
-    if (findSchedule(classList, 0, schedule, examSlots, classroomCapacities)) {
+    if (findSchedule(classList, 0, schedule, examSlots)) {
         return schedule;
     } else {
         return "No feasible schedule found";
     }
 }
 
-// Recursive function to find a feasible schedule
-function findSchedule(classList, index, schedule, examSlots, classroomCapacities) {
+// Recursive function for finding a feasible schedule
+function findSchedule(classList, index, schedule, examSlots) {
     if (index === classList.length) {
         return true;
     }
 
     for (let slot of examSlots) {
-        if (isSlotAvailable(schedule, classList[index], slot, classroomCapacities)) {
-            schedule[classList[index].CourseID] = { time: slot, professor: classList[index].ProfessorName };
-            if (findSchedule(classList, index + 1, schedule, examSlots, classroomCapacities)) {
+        if (isSlotAvailable(schedule, classList[index], slot)) {
+            schedule[classList[index].CourseID] = { time: slot, professor: classList[index].ProfessorName, student: classList[index].StudentID };
+            if (findSchedule(classList, index + 1, schedule, examSlots)) {
                 return true;
             }
             delete schedule[classList[index].CourseID];
@@ -57,12 +62,10 @@ function findSchedule(classList, index, schedule, examSlots, classroomCapacities
 
 // Main function to execute the scheduling
 function main() {
-    let classList = readCSVFile('path/to/Updated_Class_List.csv');
-    let classroomCapacities = readCSVFile('path/to/Updated_Classroom_Capacities.csv');
     let examSlots = ['9:00-10:00', '10:00-11:00', '11:00-12:00']; // Simplified exam slots
-    let examSchedule = scheduleExams(classList, examSlots, classroomCapacities);
+    let examSchedule = scheduleExams(classList, examSlots);
     console.log("Exam Schedule:", examSchedule);
 }
 
-// Run the program
 main();
+    
