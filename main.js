64e301e1fe2a2b686bb8c8881145e68da1250aa1
Dilara TOSUN <dilarat810@gmail.com>
2023-12-16 19:39:06 +0300

    I tried to add show exams according to the day and time but some there is some error in here

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 const TIME_SLOTS = ['9:00-10:00', '10:00-11:00', '11:00-12:00', '12:00-13:00', '13:00-14:00', '14:00-15:00', '15:00-16:00', '16:00-17:00', '17:00-18:00'];
+const DAYS = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
 class Classroom {
-    constructor(roomID, classroomCapacities) {
+    constructor(roomID, classroomCapacity) {
         this.roomID = roomID;
-        this.classroomCapacities = classroomCapacities;
+        this.classroomCapacity = classroomCapacity;
+        this.schedule = {};
     }
 }
 
@@ -32,50 +34,48 @@ function readFile(path, callback) {
     });
 }
 
-function isSlotAvailable(schedule, classInfo, slot, classroomCapacities) {
-    for (const key in schedule) {
-        if (schedule[key].time === slot && (schedule[key].professor === classInfo.professorName || schedule[key].student === classInfo.studentID)) {
-            return false;
-        }
-    }
+function isSlotAvailable(classroom, slot, examDay) {
+    const schedule = classroom.schedule;
 
-    const matchingRoom = classroomCapacities.find(room => room.roomID === classInfo.courseID);
-
-    if (matchingRoom && matchingRoom.classroomCapacities / 2 < classInfo.examDuration) {
+    if (schedule[examDay] && schedule[examDay][slot]) {
         return false;
     }
 
     return true;
 }
 
-function findSchedule(classList, index, schedule, examSlots, classroomCapacities) {
+function findSchedule(classList, index, classrooms, examSlots, examDays) {
     if (index === classList.length) {
         return true;
     }
 
     for (const slot of examSlots) {
-        if (isSlotAvailable(schedule, classList[index], slot, classroomCapacities)) {
-            const courseId = classList[index].courseID;
-            const { professorName, studentID } = classList[index];
+        for (const classroom of classrooms) {
+            if (isSlotAvailable(classroom, slot, examDays[index])) {
+                const courseId = classList[index].courseID;
+                const { professorName, studentID } = classList[index];
 
-            if (!schedule[courseId]) {
-                schedule[courseId] = {
-                    time: slot,
-                    professor: professorName,
-                    students: [studentID]
-                };
-            } else {
-                schedule[courseId].students.push(studentID);
-            }
+                if (!classroom.schedule[examDays[index]]) {
+                    classroom.schedule[examDays[index]] = {};
+                }
 
-            if (findSchedule(classList, index + 1, schedule, examSlots, classroomCapacities)) {
-                return true;
-            }
+                if (!classroom.schedule[examDays[index]][slot]) {
+                    classroom.schedule[examDays[index]][slot] = {
+                        courseId,
+                        professor: professorName,
+                        students: [studentID]
+                    };
 
-            schedule[courseId].students.pop();
+                    if (findSchedule(classList, index + 1, classrooms, examSlots, examDays)) {
+                        return true;
+                    }
 
-            if (schedule[courseId].students.length === 0) {
-                delete schedule[courseId];
+                    delete classroom.schedule[examDays[index]][slot];
+
+                    if (Object.keys(classroom.schedule[examDays[index]]).length === 0) {
+                        delete classroom.schedule[examDays[index]];
+                    }
+                }
             }
         }
     }
@@ -83,13 +83,19 @@ function findSchedule(classList, index, schedule, examSlots, classroomCapacities
     return false;
 }
 
-function scheduleExams(classList, examSlots, classroomCapacities) {
-    let schedule = {};
-    if (findSchedule(classList, 0, schedule, examSlots, classroomCapacities)) {
-        return schedule;
-    } else {
-        return "No feasible schedule found";
+function scheduleExams(classList, examSlots, examDays, classrooms) {
+    for (const examDay of examDays) {
+        let schedule = {};
+
+        if (findSchedule(classList, 0, classrooms, examSlots, examDays)) {
+            return classrooms.map(classroom => ({
+                roomID: classroom.roomID,
+                schedule: classroom.schedule
+            }));
+        }
     }
+
+    return "No feasible schedule found";
 }
 
 function main() {
@@ -97,12 +103,13 @@ function main() {
         const classList = classListData.map(fields => new ClassList(Number(fields[0]), fields[1], fields[2], Number(fields[3])));
 
         readFile("Classroom_Capacities.csv", function (classroomCapacitiesData) {
-            const classroomCapacities = classroomCapacitiesData.map(fields => new Classroom(fields[0], Number(fields[1])));
+            const classrooms = classroomCapacitiesData.map(fields => new Classroom(fields[0], Number(fields[1])));
 
-            let examSchedule = scheduleExams(classList, TIME_SLOTS, classroomCapacities);
+            let examSchedule = scheduleExams(classList, TIME_SLOTS, DAYS, classrooms);
             console.log("Exam Schedule:", examSchedule);
         });
     });
 }
 
 main();
+ 
