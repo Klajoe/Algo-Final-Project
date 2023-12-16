e0ed7e5df2189e11ba0817dc63f59403489d2c37
Dilara TOSUN <dilarat810@gmail.com>
2023-12-16 18:12:50 +0300

    fixed issue in Class_List.csv and main.js--Dilara

--- main.js
+++ main.js
@@ -5,6 +5,15 @@ class Classroom {
     }
 }
 
+class ClassList {
+    constructor(studentID, professorName, courseID, examDuration) {
+        this.studentID = studentID;
+        this.professorName = professorName;
+        this.courseID = courseID;
+        this.examDuration = examDuration;
+    }
+}
+
 // Function to read file
 function readFile(path, callback) {
     const fs = require("fs");
@@ -29,20 +38,21 @@ function readFile(path, callback) {
 function isSlotAvailable(schedule, classInfo, slot, classroomCapacities) {
     // Check professor and student conflicts
     for (let key in schedule) {
-        if (schedule[key].time === slot && (schedule[key].professor === classInfo.ProfessorName || schedule[key].student === classInfo.StudentID)) {
+        if (schedule[key].time === slot && (schedule[key].professor === classInfo.professorName || schedule[key].student === classInfo.studentID)) {
             return false; // Conflict found
         }
     }
 
     // Check classroom capacity constraint
-    let matchingRoom = classroomCapacities.find(room => room.RoomID === classInfo.CourseID);
+    let matchingRoom = classroomCapacities.find(room => room.roomID === classInfo.courseID);
 
-    if (matchingRoom && matchingRoom.Capacity / 2 < classInfo.ExamDuration) {
+    if (matchingRoom && matchingRoom.classroomCapacities / 2 < classInfo.examDuration) {
         return false; // Classroom capacity constraint not met
     }
 
     return true; // Slot is available
 }
+
 
 
 // Backtracking scheduler function
@@ -63,34 +73,52 @@ function findSchedule(classList, index, schedule, examSlots, classroomCapacities
 
     for (let slot of examSlots) {
         if (isSlotAvailable(schedule, classList[index], slot, classroomCapacities)) {
-            schedule[classList[index].CourseID] = { time: slot, professor: classList[index].ProfessorName, student: classList[index].StudentID };
+            const courseId = classList[index].courseID;
+
+            // Check if the courseID is already in the schedule
+            if (!schedule[courseId]) {
+                schedule[courseId] = {
+                    time: slot,
+                    professor: classList[index].professorName,
+                    students: [classList[index].studentID] // Start with an array for students
+                };
+            } else {
+                // If the courseID is already in the schedule, add the student ID
+                schedule[courseId].students.push(classList[index].studentID);
+            }
+
             if (findSchedule(classList, index + 1, schedule, examSlots, classroomCapacities)) {
                 return true;
             }
-            delete schedule[classList[index].CourseID];
+
+            // If scheduling with the current slot is unsuccessful, remove the student ID
+            schedule[courseId].students.pop();
+
+            // If no more students for the course, remove the course from the schedule
+            if (schedule[courseId].students.length === 0) {
+                delete schedule[courseId];
+            }
         }
     }
 
     return false;
 }
 
+
+
 // Main function to execute the scheduling
 function main() {
     readFile("Class_List.csv", function (classListData) {
-        const classList = classListData.map(fields => ({
-            StudentID:Number([0]),
-            ProfessorName: fields[1],
-            CourseID: fields[2],
-            ExamDuration: Number(fields[3]) // Assuming ExamDuration is a number
-        }));
+        const classList = classListData.map(fields => new ClassList(Number(fields[0]), fields[1], fields[2], Number(fields[3])));
 
         readFile("Classroom_Capacities.csv", function (classroomCapacitiesData) {
             const classroomCapacities = classroomCapacitiesData.map(fields => new Classroom(fields[0], Number(fields[1])));
 
-            let examSlots = ['9:00-10:00', '10:00-11:00', '11:00-12:00'];
+            let examSlots = ['9:00-10:00', '10:00-11:00', '11:00-12:00', '12:00-13:00', '13:00-14:00', '14:00-15:00', '15:00-16:00', '16:00-17:00', '17:00-18:00'];
             let examSchedule = scheduleExams(classList, examSlots, classroomCapacities);
             console.log("Exam Schedule:", examSchedule);
         });
+
     });
 }
 
