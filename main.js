bed965f0882e504896ddf93702abeeb24e619160
Dilara TOSUN <dilarat810@gmail.com>
2023-12-25 09:43:38 +0300

    i tried to handle undefined value -- Dilara

--- main.js
+++ main.js
@@ -28,7 +28,7 @@ function readFile(path, callback) {
         }
 
         const lines = data.split("\n");
-        const output = lines.map(line => line.split(","));
+        const output = lines.map(line => line.split(",").map(field => (field.trim() === 'undefined' ? undefined : field.trim())));
 
         callback(output);
     });
@@ -45,16 +45,19 @@ function isSlotAvailable(classroom, slot, examDay) {
 }
 
 function findSchedule(classList, index, classrooms, examSlots, examDays) {
+    // Base case: If all classes are scheduled, return true
     if (index === classList.length) {
         return true;
     }
 
+    // Iterate through available slots and classrooms
     for (const slot of examSlots) {
         for (const classroom of classrooms) {
             if (isSlotAvailable(classroom, slot, examDays[index])) {
                 const courseId = classList[index].courseID;
                 const { professorName, studentID } = classList[index];
 
+                // Create schedule entry for the class
                 if (!classroom.schedule[examDays[index]]) {
                     classroom.schedule[examDays[index]] = {};
                 }
@@ -66,10 +69,12 @@ function findSchedule(classList, index, classrooms, examSlots, examDays) {
                         students: [studentID]
                     };
 
+                    // Recursively try to schedule the next class
                     if (findSchedule(classList, index + 1, classrooms, examSlots, examDays)) {
-                        return true;
+                        return true; // If successful, return true
                     }
 
+                    // Backtrack if scheduling fails
                     delete classroom.schedule[examDays[index]][slot];
 
                     if (Object.keys(classroom.schedule[examDays[index]]).length === 0) {
@@ -80,14 +85,18 @@ function findSchedule(classList, index, classrooms, examSlots, examDays) {
         }
     }
 
-    return false;
+    return false; // No feasible schedule found
 }
 
 function scheduleExams(classList, examSlots, examDays, classrooms) {
+    // Iterate through exam days
     for (const examDay of examDays) {
+        // Create a new schedule for each exam day
         let schedule = {};
 
+        // Try to find a schedule starting from the first class
         if (findSchedule(classList, 0, classrooms, examSlots, examDays)) {
+            // If a feasible schedule is found, return the schedule
             return classrooms.map(classroom => ({
                 roomID: classroom.roomID,
                 schedule: classroom.schedule
@@ -95,7 +104,27 @@ function scheduleExams(classList, examSlots, examDays, classrooms) {
         }
     }
 
+    // If no feasible schedule is found for any exam day, return an error message
     return "No feasible schedule found";
+}
+
+function formatExamSchedule(classrooms) {
+    let formattedSchedule = "Exam Schedule:\n";
+
+    for (const day in classrooms[0].schedule) {
+        formattedSchedule += `${day}\n`;
+
+        for (const time in classrooms[0].schedule[day]) {
+            const { courseId, professor, students } = classrooms[0].schedule[day][time];
+            const studentID = students.join(',');
+
+            const formattedTime = time.replace('-', ' - ');
+
+            formattedSchedule += `${formattedTime}: ${courseId} - ${professor} - Room ${classrooms[0].roomID}\n`;
+        }
+    }
+
+    return formattedSchedule;
 }
 
 function writeToFile(filename, data) {
@@ -117,21 +146,11 @@ function main() {
 
             let examSchedule = scheduleExams(classList, TIME_SLOTS, DAYS, classrooms);
 
-            // Convert the exam schedule to a CSV-formatted string
-            let csvContent = "RoomID,Day,Time,ClassID,Professor,StudentID\n";
-            examSchedule.forEach(classroom => {
-                const roomID = classroom.roomID;
-                for (const day in classroom.schedule) {
-                    for (const time in classroom.schedule[day]) {
-                        const { courseId, professor, students } = classroom.schedule[day][time];
-                        const studentID = students.join(',');
-                        csvContent += `${roomID},${day},${time},${courseId},${professor},${studentID}\n`;
-                    }
-                }
-            });
+            // Convert the exam schedule to a formatted string
+            let formattedSchedule = formatExamSchedule(examSchedule);
 
-            // Write the CSV data to a file
-            writeToFile("Exam_Schedule.csv", csvContent);
+            // Write the formatted schedule to a file
+            writeToFile("Exam_Schedule.txt", formattedSchedule);
         });
     });
 }
