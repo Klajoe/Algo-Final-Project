3fb1266c3ab131af26811779d5204c2e6e7d2222
Enes Sapan <enessapan0@gmail.com>
2023-12-30 13:30:07 +0300

    Blocked hours returned

--- main.js
+++ main.js
@@ -7,8 +7,9 @@ const startHour = 9; // 9:00 AM
 const endHour = 18; // 6:00 PM
 
 function timeToMinutes(time) {
-    const [hour, minute] = time.split(':').map(Number);
-    return hour * 60 + minute;
+    const [hour, part] = time.split(':');
+    const baseTime = parseInt(hour) + (part.endsWith('PM') && hour !== '12' ? 12 : 0);
+    return baseTime * 60;
 }
 
 function minutesToTime(minutes) {
@@ -46,6 +47,37 @@ async function assignCoursesToRooms(coursesFilePath, roomsFilePath) {
     }));
 
     let schedule = [];
+
+    // Kullanıcıdan özel zaman dilimi bilgisi al
+    const hasSpecialTime = prompt('Özel bir zaman dilimi belirlemek istiyor musunuz? (y/n): ').toLowerCase() === 'y';
+    let specialDay, specialTime, specialCourseId;
+    if (hasSpecialTime) {
+        specialDay = prompt('Hangi gün? (Örnek: Tuesday): ');
+        specialTime = prompt('Hangi saat? (Örnek: 10:00 AM): ');
+        specialCourseId = prompt('Hangi ders? (Örnek: MATH202): ');
+    }
+
+    // Özel zaman dilimi varsa, o dersi önce yerleştir
+    if (specialDay && specialTime && specialCourseId) {
+        const specialDayIndex = days.indexOf(specialDay.charAt(0).toUpperCase() + specialDay.slice(1).toLowerCase());
+        const startTime = timeToMinutes(specialTime);
+        const specialCourse = courseDetails.find(course => course.courseId === specialCourseId);
+
+        if (specialCourse && specialDayIndex !== -1 && startTime >= startHour * 60 && startTime + specialCourse.duration <= endHour * 60) {
+            const endTime = startTime + specialCourse.duration;
+            schedule.push(`${days[specialDayIndex]},${minutesToTime(startTime)} - ${minutesToTime(endTime)}: ${specialCourse.courseId} - Room ${rooms[0].roomId}`);
+            if (specialDayIndex === 0) { // Eğer Pazartesi ise, diğer dersleri etkilememesi için bir sonraki odaya geç
+                rooms[1].nextAvailableTime = endTime;
+                rooms[1].nextAvailableDay = 0;
+            } else {
+                rooms[0].nextAvailableTime = endTime;
+                rooms[0].nextAvailableDay = specialDayIndex;
+            }
+            courseDetails.splice(courseDetails.indexOf(specialCourse), 1);
+        } else {
+            console.log('Belirtilen ders, gün veya saat hatalı.');
+        }
+    }
 
     for (const { studentId, professorName, courseId, duration } of courseDetails) {
         let assigned = false;
